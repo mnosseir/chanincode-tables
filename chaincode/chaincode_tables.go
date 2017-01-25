9d1d4a19aac32e5443e7074c904cf6654b639ca2
Nosseir <mnosseir@visa.com>
2017-01-25 13:51:21 -0800

    table restructure

--- chaincode/chaincode_tables.go
+++ chaincode/chaincode_tables.go
@@ -47,8 +47,8 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 
 	err := stub.CreateTable(args[0], []*shim.ColumnDefinition{
 		&shim.ColumnDefinition{Name: "EMP_ID", Type: shim.ColumnDefinition_STRING, Key: true},
-		&shim.ColumnDefinition{Name: "EMP_LNAME", Type: shim.ColumnDefinition_STRING, Key: true},
-		&shim.ColumnDefinition{Name: "EMP_FNAME", Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: "EMP_LNAME", Type: shim.ColumnDefinition_STRING, Key: false},
+		&shim.ColumnDefinition{Name: "EMP_FNAME", Type: shim.ColumnDefinition_STRING, Key: false},
 	})
 
 	// err := stub.PutState("hello_world", []byte(args[0]))
@@ -154,22 +154,4 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		fmt.Println("query did not find func: " + function)
 
     return nil, errors.New("Received unknown function query: " + function)
-}
-
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-    var key, jsonResp string
-    var err error
-
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
-    }
-
-    key = args[0]
-    valAsbytes, err := stub.GetState(key)
-    if err != nil {
-        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-        return nil, errors.New(jsonResp)
-    }
-
-    return valAsbytes, nil
 }
